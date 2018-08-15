4831158d18eda9f7118e4d36db22f80e6f31b789
Ben Scholer <benscholer3248511@gmail.com>
2018-08-15 16:42:36 -0400

    Using Stellar template from HTML5 Up

---
+++ assets/js/submit.js
@@ -0,0 +1,15 @@
+const TABLEID = "";
+function submit() {
+    var query = "INSERT INTO " + TABLEID + " ";
+    var data = {
+        "FirstName": "",
+        "LastName": "",
+        "Email": "",
+        "Phone": "",
+        "Gender": "",
+        "ShirtSize": "",
+        "EmergencyFirstName": "",
+        "EmergencyLastName": "",
+        "EmergencyPhone": ""
+    };
+}
